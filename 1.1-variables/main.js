ee03a57187ac566642b4f1a5fed71d9fa0cec5ce
Yulia Klyueva <liya-n@list.ru>
2020-01-22 14:39:24 +0300

    first commit

---
+++ 1.1-variables/main.js
@@ -0,0 +1,25 @@
+// Задача 1
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let averageMark = (algebra + geography + physics) / 3;
+console.log(averageMark);
+
+//Задача 2
+let myName = 'Юлия';
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+//Задача 3
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
