d69c3f4b0dc77b0b8e3624259e460fa2dfe03956
Yulia Klyueva <liya-n@list.ru>
2020-01-29 23:17:39 +0300

    first commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -12,6 +12,19 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
+	let x;
+	let discriminant = b * b - 4 * a * c;
+	// console.log(discriminant);
+	
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant == 0) {
+		x = [(-1 * b) / (2 * a)];
+		return x;
+	} else {
+		x = [((-1 * b) + Math.sqrt(discriminant)) / (2 * a) , ((-1 * b) - Math.sqrt(discriminant)) / (2 * a)];
+		return x;
+	}
     //return x;
 }
 
@@ -23,7 +36,21 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    //return averageMark;
+	let marksNew = marks;
+	let total = 0;
+	let averageMark;
+	
+	if (marks.length > 5) {
+		marksNew = marks.slice(0, 5);
+	} 
+	
+	for (let i = 0; i < marksNew.length; i++ ) {
+		total = total + marksNew[i];
+	}
+	
+	averageMark = total / marksNew.length;
+	console.log(averageMark);
+    return averageMark;
 }
 
 function calculateDrinkTask(){
@@ -35,6 +62,17 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+	let yearBirthday = dateOfBirthday.getFullYear();
+	let now = new Date();
+	let yearNow = now.getFullYear();
+	let result;
+	
+	if (yearNow - yearBirthday > 18) {
+		result = `Не желаете ли олд-фэшн, ${name}?`;
+	} else {
+		result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+	}
+	
+    console.log(result)
+    return result;
 }
