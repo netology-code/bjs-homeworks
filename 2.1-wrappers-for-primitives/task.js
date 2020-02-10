b073cf0dbb5cb813eef349e1b06e47df8b394abc
Yulia Klyueva <liya-n@list.ru>
2020-02-10 11:40:12 +0300

    first commit

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,7 @@
+"use strict";
+
+let currentDate = new Date();
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -12,7 +16,45 @@ function calculateMortgage() {
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
+	let totalAmount = 0;
+	let incorrectParameter = '';
+	let incorrectParameterValue;
+	let months = 0;
+	
+	let persentNumber = Number(percent);
+	let contributionNumber = Number(contribution);
+	let amountNumber = Number(amount);
+	
+	if (isNaN(persentNumber)) {
+		incorrectParameter = 'Процентная ставка';
+		incorrectParameterValue = percent;
+		reportIncorrectparameter(incorrectParameter, incorrectParameterValue);
+	}
+	if (isNaN(contributionNumber)) {
+		incorrectParameter = 'Начальный взнос';
+		incorrectParameterValue = contribution;
+		reportIncorrectparameter(incorrectParameter, incorrectParameterValue);
+	}
+	if (isNaN(amountNumber)) {
+		incorrectParameter = 'Общая стоимость';
+		incorrectParameterValue = amount;
+		reportIncorrectparameter(incorrectParameter, incorrectParameterValue);
+	}
+	
+	let refund = amountNumber - contributionNumber;
+	
+	console.log(typeof(date));
+	console.log(currentDate);
+	
+	//parseInt
+	
+	
     //return totalAmount;
+}
+
+//функция вывода сообщения о неправильно параметре ввода
+function reportIncorrectparameter(parameter, parameterValue) {
+	alert(`Параметр ${parameter} содержит неправильное значение ${parameterValue}`);
 }
 
 function sayHello() {
