8e396fdfe0a868ea66a34b01f379073cd937bd34
ReactORcss <57535924+ReactORcss@users.noreply.github.com>
2020-02-11 15:22:00 +0300

    Update task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,38 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal == undefined) {
+        return null;
+    }
+    let sound = "grrrr";
+    if (sound != undefined) {
+        return sound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let count = 0;
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        marks[i] = +marks[i];
+        count = count + 1;
+        sum = sum + marks[i];
+    }
+    if (sum == 0) {
+        return 0;
+    }
+    let average = sum / count;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date;
+    now = +now;
+    birthday = Date.parse(birthday);
+    const msAtYear = 31557600000; //365.25 * 24 * 60 * 60 * 1000 милисекунд в году
+    let ageGap = now - birthday;
+    if (ageGap >= msAtYear * 18) {
+        console.log('Да');
+    } else {
+        console.log('Нет');
+    }
 }
