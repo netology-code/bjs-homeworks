4107a42ebe262cabcfaee1a50ba0321d0433ba52
Vladimir_Chebukin <Vladimir_Chebukin@epam.com>
2020-05-18 21:58:37 +0300

    Update tests

--- 1.3-data-types/tests.js
+++ 1.3-data-types/tests.js
@@ -1,45 +1,45 @@
 describe('Домашнее задание к занятию 1.3 «Типы данных»', () => {
-  describe('Задача №1, должна верно считать кредит', () => {
-    it('кейс #1', () => {
+  describe('Задача №1', () => {
+    it('должна верно считать кредит: кейс #1', () => {
       const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
       expect(calculateTotalMortgage(10, 0, 50000, nextYearDate)).toEqual(52749.53);
     });
 
-    it('кейс #2', () => {
+    it('должна верно считать кредит: кейс #2', () => {
       const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
       expect(calculateTotalMortgage(10, 1000, 50000, nextYearDate)).toEqual(51694.54);
     });
 
-    it('кейс #3', () => {
+    it('должна верно считать кредит: кейс #3', () => {
       const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 4));
       expect(calculateTotalMortgage(10, 20000, 20000, nextYearDate)).toEqual(0);
     });
 
-    it('кейс #4', () => {
+    it('должна верно считать кредит: кейс #4', () => {
       const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 3));
       expect(calculateTotalMortgage(10, 0, 10000, nextYearDate)).toEqual(11616.19);
     });
 
-    it('кейс #5', () => {
+    it('должна верно считать кредит: кейс #5', () => {
       const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 3));
       expect(calculateTotalMortgage(15, 0, 10000, nextYearDate)).toEqual(12479.52);
     });
   });
 
-  describe('Задача №2 должна верно приветствовать', () => {
-    it('анонима при незаданном имени ', () => {
+  describe('Задача №2', () => {
+    it('должна верно приветствовать анонима при незаданном имени ', () => {
       expect(getGreeting()).toEqual("Привет, мир! Меня зовут Аноним");
     });
 
-    it('анонима при отсутствующем имени ', () => {
+    it('должна верно приветствовать анонима при отсутствующем имени ', () => {
       expect(getGreeting(null)).toEqual("Привет, мир! Меня зовут Аноним");
     });
 
-    it('анонима при имени равном пустой строке', () => {
+    it('должна верно приветствовать анонима при имени равном пустой строке', () => {
       expect(getGreeting("")).toEqual("Привет, мир! Меня зовут Аноним");
     });
 
-    it('пользователя при известном имени', () => {
+    it('должна верно приветствовать пользователя при известном имени', () => {
       expect(getGreeting("Пётр")).toEqual("Привет, мир! Меня зовут Пётр");
     });
   });
--- 3.3-async/tests.js
+++ 3.3-async/tests.js
@@ -6,47 +6,47 @@ describe('Домашнее задание к занятию 3.3 «Асинхро
     clock = new AlarmClock();
   }) 
 
-  it('Необходимо создавать объект будильник', () => {
+  it('необходимо создавать объект будильник', () => {
     expect(clock).toBeTruthy();
   });
 
-  it('Необходимо создавать добавлять звонок', () => {
+  it('необходимо создавать добавлять звонок', () => {
     clock.addClock("16:45", f => f, 1);
     expect(clock.alarmCollection.length).toEqual(1);
   });
 
-  it('Необходимо добавлять и удалять звонок', () => {
+  it('необходимо добавлять и удалять звонок', () => {
     clock.addClock("16:45", f => f, 1);
     expect(clock.alarmCollection.length).toEqual(1);
     clock.removeClock(1);
     expect(clock.alarmCollection.length).toEqual(0);
   });
 
-  it('Id интервала должно отсутствовать до запуска', () => {
+  it('id интервала должно отсутствовать до запуска', () => {
     expect(clock.timerId).toBeNull();
   });
 
-  it('Необходимо запускать будильник', () => {
+  it('необходимо запускать будильник', () => {
     clock.addClock("16:45", f => f, 1);
     clock.start();
     expect(clock.timerId).toBeDefined();
   });
 
-  it('Будильник должен возвращать время в формате HH:MM', () => {
+  it('будильник должен возвращать время в формате HH:MM', () => {
     const currentDate = new Date();
     const hours = currentDate.getHours() < 10 ? `0${currentDate.getHours()}` : `${currentDate.getHours()}`;
     const minutes = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : `${currentDate.getMinutes()}`;
     expect(clock.getCurrentFormattedTime()).toEqual(`${hours}:${minutes}`);
   });
 
-  it('Будильник должен создавать таймер, а затем его удалять', () => {
+  it('будильник должен создавать таймер, а затем его удалять', () => {
     clock.start();
     expect(clock.timerId).toBeDefined();
     clock.stop();
     expect(clock.timerId).toBeNull();
   });
 
-  it('Будильник должен создавать звонки, а затем очищать из все', () => {
+  it('будильник должен создавать звонки, а затем очищать из все', () => {
     clock.addClock("16:45", f => f, 1);
     clock.addClock("16:45", f => f, 2);
     clock.addClock("16:45", f => f, 3);
@@ -55,13 +55,13 @@ describe('Домашнее задание к занятию 3.3 «Асинхро
     expect(clock.alarmCollection.length).toEqual(0);
   });
 
-  it('Будильник не должен создавать таймер с одинаковым id', () => {
+  it('будильник не должен создавать таймер с одинаковым id', () => {
     clock.addClock("16:45", f => f, 1);
     clock.addClock("16:45", f => f, 1);
     expect(clock.alarmCollection.length).toEqual(1);
   });
 
-  it('Будильник должен выбрасывать объект ошибки, если id не был передан', () => {
+  it('будильник должен выбрасывать объект ошибки, если id не был передан', () => {
     expect(() => clock.addClock("16:45", f => f)).toThrow();
   });
 
